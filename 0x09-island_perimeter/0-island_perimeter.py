db39da2b70c97c3b9f12dcac925e1268a755aee7
Mohamed <mdahni06@gmail.com>
2024-12-08 16:51:23 +0100

    +

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""Define the island perimeter finding function"""
+"""Define the island perimeter finding function."""
 
 
 def island_perimeter(grid):
-    """Count the perimeter of an island
+    """Count the perimeter of an island.
 
     Args:
-        grid (list): A list of lists of integers representing an island
-
-    Return:
-        The perimeter of the island defined in the grid
+        grid (list): A list of lists of integers representing an island.
+    Returns:
+        int: The perimeter of the island defined in the grid.
     """
-    # difine the perimeter variable
     perimeter = 0
 
     for ri, row in enumerate(grid):
         for ci, column in enumerate(row):
             if column:
-                # check top part
+                # Check top part
                 if ri == 0 or grid[ri - 1][ci] == 0:
-                    perimeter = perimeter + 1
-                # check left part
+                    perimeter += 1
+                # Check left part
                 if ci == 0 or grid[ri][ci - 1] == 0:
-                    perimeter = perimeter + 1
-                # check right part
-                if ci == ri + 1 or grid[ri][ci + 1] == 0:
-                    perimeter = perimeter + 1
-                # check bottom part
+                    perimeter += 1
+                # Check right part
+                if ci == len(row) - 1 or grid[ri][ci + 1] == 0:
+                    perimeter += 1
+                # Check bottom part
                 if ri == len(grid) - 1 or grid[ri + 1][ci] == 0:
-                    perimeter = perimeter + 1
+                    perimeter += 1
 
-    # return the perimeter
     return perimeter
