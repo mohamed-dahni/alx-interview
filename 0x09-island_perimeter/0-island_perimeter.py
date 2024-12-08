574509cef9698139c25a4444e0fdf80e8e09fc22
Mohamed <mdahni06@gmail.com>
2024-12-08 16:29:16 +0100

    +

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """Define the island perimeter finding function"""
 
+
 def island_perimeter(grid):
     """Count the perimeter of an island
 
     Args:
         grid (list): A list of lists of integers representing an island
 
-    Return: 
+    Return:
         The perimeter of the island defined in the grid
     """
-    
+
     perimeter = 0
-    
+
     for ri, row in enumerate(grid):
         for ci, column in enumerate(row):
             if column:
@@ -28,6 +29,6 @@ def island_perimeter(grid):
                 # check bottom part
                 if ri == len(grid) - 1 or grid[ri + 1][ci] == 0:
                     perimeter = perimeter + 1
-    
+
     return perimeter
 
