b495198fe8e6610638dceccb98fa4a4c16a36dab
Mohamed <mdahni06@gmail.com>
2024-12-15 20:37:36 +0100

    +

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for prime game"""
 
+
 def sieve_of_eratosthenes(n):
     """Generate a list of primes up to n using the Sieve of Eratosthenes."""
     sieve = [True] * (n + 1)
@@ -11,11 +12,14 @@ def sieve_of_eratosthenes(n):
                 sieve[multiple] = False
     return [num for num, is_prime in enumerate(sieve) if is_prime]
 
+
 def simulate_game(n):
-    """Simulate a game for the given n and return the winner ('Maria' or 'Ben')."""
+    """
+    Simulate a game for the given n and return the winner ('Maria' or 'Ben').
+    """
     primes = sieve_of_eratosthenes(n)
     nums = [True] * (n + 1)  # True means the number is still in the game
-    
+
     # Remove multiples of primes
     for prime in primes:
         for multiple in range(prime, n + 1, prime):
@@ -30,16 +34,17 @@ def simulate_game(n):
                 for multiple in range(prime, n + 1, prime):
                     nums[multiple] = False
                 break  # Exit loop once a move is made
-        
+
         turn = 1 - turn  # Switch turns
-    
+
     # If turn == 0, Maria has won (because Ben couldn't play)
     return 'Ben' if turn == 0 else 'Maria'
+
 
 def isWinner(x, nums):
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         if n <= 1:  # Handle case where there are no primes
             ben_wins += 1
@@ -49,11 +54,10 @@ def isWinner(x, nums):
                 maria_wins += 1
             else:
                 ben_wins += 1
-    
+
     if maria_wins > ben_wins:
         return 'Maria'
     elif ben_wins > maria_wins:
         return 'Ben'
     else:
         return None
-
