a97572ee4bb8b26793cd08fa19e4733b6fbc7657
Mohamed <mdahni06@gmail.com>
2024-12-15 20:46:35 +0100

    +

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,73 +1,42 @@
 #!/usr/bin/python3
-"""Module for prime game"""
+"""0. Prime Game - Maria and Ben are playing a game"""
 
-
-def sieve_of_eratosthenes(n):
-    """Generate a list of primes up to n using the Sieve of Eratosthenes."""
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False  # 0 and 1 are not primes
-    for start in range(2, int(n**0.5) + 1):
-        if sieve[start]:
-            for multiple in range(start * start, n + 1, start):
-                sieve[multiple] = False
-    return [num for num, is_prime in enumerate(sieve) if is_prime]
-
-def simulate_game(n):
-    """Simulate a game for the given n and return the winner ('Maria' or 'Ben')."""
-    primes = sieve_of_eratosthenes(n)
-    nums = [True] * (n + 1)  # True means the number is still in the game
-
-    # Remove multiples of primes
-    for prime in primes:
-        for multiple in range(prime, n + 1, prime):
-            nums[multiple] = False  # Mark as removed
-
-    # Game simulation, alternating turns between Maria and Ben
-    turn = 0  # 0 for Maria, 1 for Ben
-    while True:
-        # Find the next available prime number
-        available_prime = None
-        for prime in primes:
-            if nums[prime]:
-                available_prime = prime
-                break
-        
-        if available_prime is None:
-            # No primes left to play
-            break
-
-        # Remove the prime and its multiples
-        for multiple in range(available_prime, n + 1, available_prime):
-            nums[multiple] = False
-
-        # Switch turns
-        turn = 1 - turn
-
-    # If turn == 0, Maria has won (because Ben couldn't play)
-    return 'Ben' if turn == 0 else 'Maria'
 
 def isWinner(x, nums):
+    """x - rounds
+    nums - numbers list
     """
-    Decide the winner
-    """
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if n <= 1:  # Handle case where there are no primes
-            ben_wins += 1
-        else:
-            winner = simulate_game(n)
-            if winner == 'Maria':
-                maria_wins += 1
-            else:
-                ben_wins += 1
-    
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
-        return 'Ben'
-    else:
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
         return None
 
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
